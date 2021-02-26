31fdb905d4113bfe48ec14ba7b704a438f23b27d
StevennYau <stevenyau@gmail.com>
2021-02-26 18:54:04 -0500

    added crud methods for business service and business entity

---
+++ src/app/entities/business.ts
@@ -0,0 +1,12 @@
+export interface Business {
+   id: number;
+   address_id: number;
+   alias: string;
+   image_url: string;
+   is_closed: boolean;
+   name: string;
+   phone: string;
+   rating: number;
+   review_count: number;
+   url: string;
+} // this is the type of data that is gonna be returned
--- src/app/services/business.service.ts
+++ src/app/services/business.service.ts
@@ -1,10 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Business } from '../entities/business';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BusinessService {
+  private apiServerUrl = environment.apiBaseUrl;
 
   constructor(private http: HttpClient) { }
-}
+
+  public getBusiness(): Observable<Business[]> {
+     return this.http.get<Business[]>(`${this.apiServerUrl}/businesses`);
+  }
+
+  public addBusiness(business: Business): Observable<Business> {
+     return this.http.post<Business>(`${this.apiServerUrl}/addBusiness`, business);
+  }
+
+  public updateBusiness(business: Business): Observable<Business> {
+     return this.http.put<Business>(`${this.apiServerUrl}/updateBusiness`, business);
+  }
+
+  public deleteBusiness(businessId: number): Observable<void> {
+     return this.http.delete<void>(`${this.apiServerUrl}/deleteBusiness/${businessId}`);
+  }
+}         
